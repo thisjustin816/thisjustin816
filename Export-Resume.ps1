f1c89484d78a15d222266d1536b69350f0ddef53
Justin Beeson <jbeeson816@gmail.com>
2021-12-13 20:13:54 -0800

    fix export script

--- Export-Resume.ps1
+++ Export-Resume.ps1
@@ -1,27 +1,23 @@
-[CMdletBinding()]
+[CmdletBinding()]
 param (
-    [String]$Name,
-    [String]$Source = "$PSScriptRoot\README.md",
+    [String]$Name = $env:USERNAME,
+    [String]$FilePath = "$PSScriptRoot\README.md",
     [String]$Destination = $PSScriptRoot
 )
 
-if (!( Test-Path -Path $Destination -ErrorAction SilentlyContinue )) {
-    New-Item -Path $Destination -ItemType Directory -Force
-}
-else {
-    Get-ChildItem -Path $Destination |
-        Where-Object -FilterScript { $_.Name -match '.*-resume\.(pdf|txt)' } |
-        Remove-Item -Force
-}
+$source = Get-Item -Path $FilePath
 
 Start-Process -FilePath 'npm' -ArgumentList 'i -g md-to-pdf' -NoNewWindow -Wait
-Start-Process -FilePath 'md-to-pdf' -ArgumentList $Source -NoNewWindow -Wait
+Start-Process -FilePath 'md-to-pdf.cmd' -ArgumentList $source.FullName -NoNewWindow -Wait
 
-$pdfResume = Get-ChildItem -Path $PSScriptRoot -Filter '*.pdf' -Recurse |
+$pdfResume = Get-ChildItem -Path $source.Parent.FullName -Filter '*.pdf' -Recurse |
     Rename-Item -NewName "$Name-resume.pdf" -PassThru -Force
 
 if (!( Get-ChildItem -Path $Destination -Filter $pdfResume.Name )) {
     Move-Item -Path $pdfResume.FullName -Destination $Destination -PassThru -Force
 }
+else {
+    $pdfResume
+}
 
-Get-Item -Path $Source | Copy-Item -Destination "$Destination\$Name-resume.txt" -PassThru -Force
+$source | Copy-Item -Destination "$Destination\$Name-resume.txt" -PassThru -Force
